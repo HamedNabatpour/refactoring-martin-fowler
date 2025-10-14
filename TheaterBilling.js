e165c0cf5b80b95cc6da5c2be241acce76a6e669
hamed.nabatpour <hamed.np.fhy91@gmail.com>
2025-10-14 21:38:31 +0330

    inital two phases refactoring

--- TheaterBilling.js
+++ TheaterBilling.js
@@ -3,76 +3,91 @@ function playFor(perf) {
 }
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
+    return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
-
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-
         // print line for this order (with newline)
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
-        totalAmount += amountFor(perf);
+    }
+    result += `Amount owed is ${usd(totalAmount())}\n`;
+    result += `You earned ${totalVolumeCredits()} credits\n`;
+    return result;
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
     }
 
-    result += `Amount owed is ${usd(totalAmount)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
-    return result;
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    }
+
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency", currency: "USD", minimumFractionDigits: 2
+        }).format(aNumber / 100);
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if (comedy === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    function amountFor(aPerformance) {
+        let result;
+        switch (playFor(aPerformance).type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+
+            case comedy:
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+
+            default:
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+        }
+        return result;
+    }
+
 }
 
 const comedy = "comedy";
-
-function usd(aNumber){
-    return new Intl.NumberFormat("en-US", {
-        style: "currency", currency: "USD", minimumFractionDigits: 2
-    }).format(aNumber / 100);
-}
-function volumeCreditsFor(aPerformance){
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if (comedy === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-    return result;
-}
-
-function amountFor(aPerformance){
-    let result;
-    switch (playFor(aPerformance).type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-
-        case comedy:
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-
-        default:
-            throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-    }
-    return result;
-}
-
-
 const plays = {
-    hamlet: { name: "Hamlet", type: "tragedy" },
-    aslike: { name: "As You Like It", type: comedy },
-    othello: { name: "Othello", type: "tragedy" }
+    hamlet: {name: "Hamlet", type: "tragedy"},
+    aslike: {name: "As You Like It", type: comedy},
+    othello: {name: "Othello", type: "tragedy"}
 }
 
 const invoice = {
     customer: "BigCo",
     performances: [
-        { playID: "hamlet", audience: 55 },
-        { playID: "aslike", audience: 35 },
-        { playID: "othello", audience: 40 }
+        {playID: "hamlet", audience: 55},
+        {playID: "aslike", audience: 35},
+        {playID: "othello", audience: 40}
     ]
 }
+
 
 console.log(statement(invoice, plays))
 
